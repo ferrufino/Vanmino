f4ed12b08bdf10b393be477d867387b2ea89b7f5
Gustavo Ferrufino <ferrufino@Gustavos-MacBook-Pro.local>
2018-12-02 14:02:07 -0800

    Initial Commit

---
+++ Vanmino/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Vanmino
+//
+//  Created by Gustavo Ferrufino on 2018-12-02.
+//  Copyright © 2018 Gustavo Ferrufino. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
