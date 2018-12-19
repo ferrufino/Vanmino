a692127110178a5a4fec3d649eb5fabbe2c2ca48
Gustavo Ferrufino <ferrufino@Gustavos-MBP.hitronhub.home>
2018-12-19 11:09:52 -0800

    Add git ignore

---
+++ Enums/TrailType.swift
@@ -0,0 +1,10 @@
+//
+//  TrailType.swift
+//  Vanmino
+//
+//  Created by Gustavo Ferrufino on 2018-12-02.
+//  Copyright © 2018 Gustavo Ferrufino. All rights reserved.
+//
+
+import Foundation
+
---
+++ View/TrailCell.swift
@@ -0,0 +1,24 @@
+//
+//  TrailCell.swift
+//  Vanmino
+//
+//  Created by Gustavo Ferrufino on 2018-12-02.
+//  Copyright © 2018 Gustavo Ferrufino. All rights reserved.
+//
+
+import UIKit
+
+class TrailCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
